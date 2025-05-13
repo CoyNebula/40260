2efa958a344a238df8330ab03b0cd252e2df4399
Luis <Comega02>
2025-05-12 23:50:42 -0600

    refacturación

--- html00/formulario.js
+++ html00/formulario.js
@@ -1,15 +1,41 @@
-console.log("Inicio");
+console.log("inicio")
 
-const boton = document.getElementById("Enviar")
-
-console.log(boton);
+/*
+const boton = document.getElementById("enviar")
 
 boton.addEventListener("click", 
-    () => {
-        console.log("los parametros")
-        let user = document.getElementById("user")
-        let password = document.getElementById("password")
-        console.log(user.value)
+    ()=> {
+        console.log("los parámetros")
+        let user=document.getElementById("user")
+        let password=document.getElementById("password")
         console.log(password.value)
+        console.log(user.value)
     }
 )
+*/
+
+/*function envia(){
+    console.log("los parámetros")
+    let user=document.getElementById("user")
+    let password=document.getElementById("password")
+    console.log(password.value)
+    console.log(user.value)
+
+}*/
+
+const mostrar = () => {
+    let user = document.getElementById("user")
+    let password = document.getElementById("password")
+    console.log(user.value)
+    console.log(password.value)
+
+    let formulario = document.getElementsByTagName("form")
+    console.log(formulario)
+    user.setAttribute("name", "q")
+    password.setAttribute("name", "password")
+    formulario[0].setAttribute("action", "https://google.com")
+    formulario[0].submit()
+}
+
+const buton = document.getElementById("enviar")
+buton.addEventListener("click", mostrar)
